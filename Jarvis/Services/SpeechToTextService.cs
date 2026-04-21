0f90719b40b9ff09696aea2b6e334227949289b3
Student <Student@DESKTOP-FPC590P>
2026-04-21 13:35:51 +0300

    Скачал библиотеки для Whisper и создал класс  SpeechToTextServiceSpeechToTextService

---
+++ Jarvis/Services/SpeechToTextService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Whisper.net;
+using Whisper.net.Ggml;
+
+
+namespace Jarvis.Services
+{
+    public class SpeechToTextService
+    {
+
+    }
+}
