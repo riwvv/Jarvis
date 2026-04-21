71773b79f64aa57cb1af5f217ecf3d23f87c511e
Student <Student@DESKTOP-IL21489>
2026-04-21 14:37:28 +0300

    Добавил модель InstalledApplication, удалил TempModel

---
+++ Jarvis/Models/InstalledApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarvis.Models;
+
+public class InstalledApplication
+{
+    public string? DisplayName { get; set; }
+    public string? DisplayVersion { get; set; }
+    public string? Publisher { get; set; }
+    public string? InstallLocation { get; set; }
+    public string? DisplayIcon { get; set; }
+    public string? UninstallString { get; set; }
+    public string? ExecutablePath { get; set; }
+}
+
