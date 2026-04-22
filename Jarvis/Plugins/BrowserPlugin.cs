6947445a18587d1c09a6bf2ba0a16ffd908e0526
Student <Student@DESKTOP-IL21489>
2026-04-22 14:31:20 +0300

    Добавил BrowserPlugin

---
+++ Jarvis/Plugins/BrowserPlugin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.ComponentModel;
+using System.Security.Policy;
+
+namespace Jarvis.Plugins;
+public class BrowserPlugin
+{
+    [KernelFunction]
+    [Description("Ищет информацию в Google и открывает результаты в браузере")]
+    public string SearchGoogle([Description("Поисковый запрос или URL")] string query)
+    {
+        try
+        {
+            return $"Открыт URL - ";
+        }
+        catch(Exception ex)
+        {
+            return $"Ошибка при поиске - {ex.Message}";
+        }
+    }
+}
+
