31f1040714a96ed80b14381223d4eaedf3dd891e
Student <Student@DESKTOP-IL21489>
2026-04-22 11:58:18 +0300

    Добавил SystemAudioPlugin и зарегал его в SemanticKernel

---
+++ Jarvis/Plugins/SystemAudioPlugin.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.SemanticKernel;
+using NAudio.CoreAudioApi;
+using System.ComponentModel;
+
+namespace Jarvis.Plugins;
+public class SystemAudioPlugin
+{
+    [KernelFunction]
+    [Description("Изменение уровня громкости системы")]
+    public string ChangeVolume([Description("Значение от 0.0 до 1.0. Например: 'громкость 50%' значит 0.5f")] float volume)
+    {
+        try
+        {
+            var devices = new MMDeviceEnumerator();
+            var device = devices.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+
+            return "Done";
+        }
+        catch
+        {
+            return "Error";
+        }
+    }
+
+    [KernelFunction]
+    [Description("Отключение громкости системы")]
+    public string VolumeTurnOff()
+    {
+        try
+        {
+            var devices = new MMDeviceEnumerator();
+            var device = devices.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.Mute = true;
+
+            return "Done";
+        }
+        catch
+        {
+            return "Error";
+        }
+    }
+}
+
