b82b7ea12112acf460724211df3aef9a25cac559
riwvv <ionovandrey2014@gmail.com>
2026-04-27 00:52:11 +0300

    последняя рабочая версия

--- Jarvis/App.xaml.cs
+++ Jarvis/App.xaml.cs
@@ -26,7 +26,7 @@ public partial class App : Application {
         builder.Plugins.AddFromType<SystemAudioPlugin>();
         builder.Plugins.AddFromType<BrowserPlugin>();
 
-        builder.AddOpenAIChatCompletion(modelId: "qwen2.5:7b", endpoint: new Uri("http://localhost/11434/v1"), apiKey: "dummy");
+        builder.AddOpenAIChatCompletion(modelId: "qwen2.5:7b", endpoint: new Uri("http://localhost:11434/v1"), apiKey: "dummy");
         KernelCore = builder.Build();
     }
 
