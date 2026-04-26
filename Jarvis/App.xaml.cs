515afd17e9a8e247ffff0548402570443f17b84a
riwvv <ionovandrey2014@gmail.com>
2026-04-27 00:24:48 +0300

    Revert "Dany"

--- Jarvis/App.xaml.cs
+++ Jarvis/App.xaml.cs
@@ -52,7 +52,6 @@ public partial class App : Application {
         mainWindow.DataContext = _host.Services.GetRequiredService<MainViewModel>();
         mainWindow.Show();
         base.OnStartup(e);
-        Application.Current.MainWindow.Hide();
     }
 
     protected override async void OnExit(ExitEventArgs e) {
@@ -63,7 +62,7 @@ public partial class App : Application {
             if (proc.Id != Environment.ProcessId)
                 proc.Kill();
         }
-        
+
         base.OnExit(e);
     }
 }
