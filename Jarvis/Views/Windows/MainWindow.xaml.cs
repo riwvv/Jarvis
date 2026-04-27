72aad197d698e0bfadf7685db3b551730b061502
Dany-223 <dansalakhov@gmail.com>
2026-04-27 13:23:24 +0300

    расположение окна

--- Jarvis/Views/Windows/MainWindow.xaml.cs
+++ Jarvis/Views/Windows/MainWindow.xaml.cs
@@ -7,6 +7,13 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.WindowStartupLocation = WindowStartupLocation.Manual;
+
+        var screenWidth = SystemParameters.WorkArea.Width;
+        var screenHeight = SystemParameters.WorkArea.Height;
+
+        this.Left = screenWidth - this.Width;
+        this.Top = screenHeight - this.Height;
     }
 
     private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
