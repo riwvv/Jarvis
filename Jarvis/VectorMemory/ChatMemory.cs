d7414ef220d018efb2c084e995dd329cc83bd78c
riwvv <ionovandrey2014@gmail.com>
2026-05-02 04:00:45 +0300

    первая рабочая версия долгосрочной памяти готова

--- Jarvis/VectorMemory/ChatMemory.cs
+++ Jarvis/VectorMemory/ChatMemory.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.VectorData;
 
-namespace Jarvis.VectorMamory;
+namespace Jarvis.VectorMemory;
 
 public class ChatMemory {
     [VectorStoreKey] public string Id { get; init; } = Guid.NewGuid().ToString();
-    [VectorStoreData] public required string IserPrompt { get; init; }
+    [VectorStoreData] public required string UserPrompt { get; init; }
     [VectorStoreData] public required string AssistantResponse { get; init; }
     [VectorStoreData] public required string Timestamp { get; init; }
-    [VectorStoreVector(2880)] public ReadOnlyMemory<float> Embedding { get; set; }
+    [VectorStoreVector(384)] public ReadOnlyMemory<float> Embedding { get; set; }
 }
